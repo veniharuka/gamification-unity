ec6c53b511800f82c8a1abe213b27905f64f1ec2
Choi hojin <jkhh9725@gmail.com>
2024-11-10 20:46:08 +0900

    Feat: B23 대화 시스템 구현

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,28 +1,51 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 // 대화창 UI 구축하기 [유니티 기초 강좌 B22] - 호진
 // PlayerAction 과 연결되어 있음. 
 public class GameManager :  MonoBehaviour
 {
-
+    public TalkManager talkManager;
     public GameObject talkPanel;
+    public Image protraitImg;
     public TextMeshProUGUI talkText;
     public GameObject scanObject;
     public bool isAction;
-
+    public int talkIndex; 
     public void Action(GameObject scanObj) 
     {
-        if (isAction){
+       scanObject = scanObj;
+       ObjData objData = scanObject.GetComponent<ObjData>();
+       Talk(objData.id, objData.isNpc);
+
+       talkPanel.SetActive(isAction);
+    }
+
+    void Talk(int id, bool isNpc)
+    {
+      string talkData = talkManager.GetTalk(id, talkIndex);
+
+        if (talkData == null) // 대화가 더 이상 없으면 종료
+        {
             isAction = false;
-        }
-        else {
-            isAction = true;
-            scanObject = scanObj;
-            talkText.text = "이것의 이름은" + scanObj.name + "이라고 한다";
+            talkIndex = 0; // 이야기가 종료되면 초기화. 
+            return;
         }
 
-        talkPanel.SetActive(isAction);
-       
+        if (isNpc)
+        {
+            talkText.text = talkData;
+
+            protraitImg.color = new Color(1, 1, 1, 1); // npc라면 투명도 1(드러냄)
+        }
+        else { 
+            talkText.text = talkData;
+            protraitImg.color = new Color(1, 1, 1, 0); // npc가 아니라면 투명도 0(숨김) 
+
+        }
+        isAction = true;
+        talkIndex++; // 문장 뽑아내기. 
     }
 }
