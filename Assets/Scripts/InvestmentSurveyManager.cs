826949e656744af4123d4659f7f5b067129b2b40
veniharuka <kurobas1384@gmail.com>
2024-11-17 23:52:57 +0900

    Chore: prefab 수정
    -SurveyPanel 하위의 GradePanel과 ContentPanel
    수정 후 prefabs 추가

--- Assets/Scripts/InvestmentSurveyManager.cs
+++ Assets/Scripts/InvestmentSurveyManager.cs
@@ -81,7 +81,7 @@ public class InvestmentSurveyManager : MonoBehaviour
     {
         if (answerPanel == null || answerButtonPrefab == null) return;
 
-        // 기존 버튼 모두 제거
+        // 기존 버튼들 제거
         foreach (Transform child in answerPanel.transform)
         {
             Destroy(child.gameObject);
@@ -89,26 +89,58 @@ public class InvestmentSurveyManager : MonoBehaviour
 
         // 예 버튼 생성
         yesButton = Instantiate(answerButtonPrefab, answerPanel.transform);
+        // 컴포넌트들 명시적으로 활성화
+        Image yesImage = yesButton.GetComponent<Image>();
+        if (yesImage != null)
+        {
+            yesImage.enabled = true;
+            yesImage.raycastTarget = true;
+        }
+
+        Button yesButtonComp = yesButton.GetComponent<Button>();
+        if (yesButtonComp != null)
+        {
+            yesButtonComp.enabled = true;
+            yesButtonComp.interactable = true;
+        }
+
         RectTransform yesRect = yesButton.GetComponent<RectTransform>();
-        yesRect.sizeDelta = new Vector2(230f, 50f);  // 너비 230, 높이 50으로 설정
+        yesRect.sizeDelta = new Vector2(230f, 50f);
+
         TextMeshProUGUI yesText = yesButton.GetComponentInChildren<TextMeshProUGUI>();
         if (yesText != null)
         {
+            yesText.enabled = true;
             yesText.text = "예";
-            //yesText.fontSize = 50;
-            yesText.alignment = TextAlignmentOptions.Center; // 텍스트 중앙 정렬
+            yesText.alignment = TextAlignmentOptions.Center;
         }
         yesButton.onClick.AddListener(() => SelectAnswer(0));
 
-        // 아니오 버튼 생성
+        // 아니오 버튼도 동일한 방식으로 처리
         noButton = Instantiate(answerButtonPrefab, answerPanel.transform);
+        Image noImage = noButton.GetComponent<Image>();
+        if (noImage != null)
+        {
+            noImage.enabled = true;
+            noImage.raycastTarget = true;
+        }
+
+        Button noButtonComp = noButton.GetComponent<Button>();
+        if (noButtonComp != null)
+        {
+            noButtonComp.enabled = true;
+            noButtonComp.interactable = true;
+        }
+
         RectTransform noRect = noButton.GetComponent<RectTransform>();
-        noRect.sizeDelta = new Vector2(230f, 50f);  // 너비 230, 높이 50으로 설정
+        noRect.sizeDelta = new Vector2(230f, 50f);
+
         TextMeshProUGUI noText = noButton.GetComponentInChildren<TextMeshProUGUI>();
         if (noText != null)
         {
+            noText.enabled = true;
             noText.text = "아니오";
-            noText.alignment = TextAlignmentOptions.Center; // 텍스트 중앙 정렬
+            noText.alignment = TextAlignmentOptions.Center;
         }
         noButton.onClick.AddListener(() => SelectAnswer(1));
     }
